57500535fab07757f1d415f3712bba487665a989
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-19 19:58:55 +0100

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 const CONFIG = require('./config.example');
 
-if (CONFIG.botToken !== '') 
+if (client.login(process.env.BOT_TOKEN) !== '') 
 	throw new Error("Please remove the Discord bot token from 'botToken' in the config.js file.");
 if (CONFIG.yourID !== '')
 	throw new Error("Please remove the user ID from 'yourID' in the config.js file.");
